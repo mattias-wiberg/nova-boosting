79f4342815a6e65df7b40aa72892692137089d4a
Mattias Wiberg <klutten99@gmail.com>
2023-02-19 19:44:53 +0100

    Moved api key over to .env file.

--- src/firebase.js
+++ src/firebase.js
@@ -5,11 +5,11 @@ import { getStorage } from 'firebase/storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB4dUeEO5GyjiOWUKJK2Q7cPfv0McFLras",
-  authDomain: "auth-mini.firebaseapp.com",
-  projectId: "auth-mini",
-  storageBucket: "auth-mini.appspot.com",
-  messagingSenderId: "487291698458",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
   appId: "1:487291698458:web:52df0e8c38901d0840c0a3"
 };
 
