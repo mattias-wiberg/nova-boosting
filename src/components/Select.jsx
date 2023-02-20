faa8b84e59d603367cd2c00da66987fa9896d671
Mattias Wiberg <klutten99@gmail.com>
2023-02-20 19:32:29 +0100

    Select component done.

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,63 +1,110 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import ExpandLessIcon from "@mui/icons-material/ExpandLess";
 import "../style/css/components/select.scss";
 
-const Select = ({}) => {
+const Select = ({ multiple, className }) => {
   const [state, setState] = useState({
-    location: [
+    characters: [
       {
         id: 0,
-        title: "New York",
+        name: "Mathew",
+        realm: "Deathwing",
+        class: "warrior",
         selected: false,
-        key: "location",
+        key: "characters",
       },
       {
         id: 1,
-        title: "Dublin",
+        name: "Drulu",
+        realm: "Deathwing",
+        class: "druid",
         selected: false,
-        key: "location",
+        key: "characters",
       },
       {
         id: 2,
-        title: "California",
+        name: "Thrallin",
+        realm: "Deathwing",
+        class: "demon-hunter",
         selected: false,
-        key: "location",
-      },
-      {
-        id: 3,
-        title: "Istanbul",
-        selected: false,
-        key: "location",
-      },
-      {
-        id: 4,
-        title: "Izmir",
-        selected: false,
-        key: "location",
-      },
-      {
-        id: 5,
-        title: "Oslo",
-        selected: false,
-        key: "location",
+        key: "characters",
       },
     ],
   });
   const [expanded, setExpanded] = useState(false);
-  const [selected, setSelected] = useState({});
 
-  const expand = () => {
+  useEffect(() => {
+    setTimeout(() => {
+      if (expanded) {
+        window.addEventListener("click", close);
+      } else {
+        window.removeEventListener("click", close);
+      }
+    }, 0);
+
+    return () => {
+      window.removeEventListener("click", close);
+    };
+  }, [expanded]);
+
+  const expand = (e) => {
+    e.preventDefault();
     setExpanded(!expanded);
   };
 
+  const resetThenSet = (id, key) => {
+    const temp = [...state[key]];
+
+    temp.forEach((item) => (item.selected = false));
+    temp[id].selected = true;
+
+    setState({
+      [key]: temp,
+    });
+    setExpanded(false);
+    console.log(state);
+  };
+
+  const toggleItem = (id, key) => {
+    const temp = [...state[key]];
+
+    temp[id].selected = !temp[id].selected;
+
+    setState({
+      [key]: temp,
+    });
+  };
+
+  const close = () => {
+    setExpanded(false);
+  };
+
   return (
-    <div className="select" onClick={expand}>
-      <div className="left">
-        <span className="selected">Faze Clan</span>
-      </div>
-      <div className="right">
-        <span className="label">3/4</span>
+    <div className={"select" + className && " " + className}>
+      <div className="header" onClick={expand}>
+        {state.characters.filter((item) => item.selected).length > 0 ? (
+          multiple ? (
+            <span className="selected">
+              {"Selected " +
+                state.characters.filter((item) => item.selected).length}
+            </span>
+          ) : (
+            <div>
+              <span
+                className={
+                  state.characters.filter((item) => item.selected)[0].class
+                }
+              >
+                {state.characters.filter((item) => item.selected)[0].name}
+              </span>
+              {" - " +
+                state.characters.filter((item) => item.selected)[0].realm}
+            </div>
+          )
+        ) : (
+          <span className="placeholder">Select</span>
+        )}
         <div className="arrow">
           {expanded ? (
             <ExpandLessIcon fontSize="inherit" />
@@ -66,6 +113,24 @@ const Select = ({}) => {
           )}
         </div>
       </div>
+      {expanded && (
+        <div className="items">
+          {state.characters.map((item) => (
+            <div
+              className={`item ${item.selected && "selected"}`}
+              key={item.id}
+              onClick={() =>
+                multiple
+                  ? toggleItem(item.id, item.key)
+                  : resetThenSet(item.id, item.key)
+              }
+            >
+              <span className={item.class}>{item.name}</span>
+              {" - " + item.realm}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
--- src/pages/Teams.jsx
+++ src/pages/Teams.jsx
@@ -3,6 +3,8 @@ import Button from "../components/Button";
 import Select from "../components/Select";
 import Team from "../components/Team";
 import { AuthContext } from "../context/AuthContext";
+import AddIcon from "@mui/icons-material/Add";
+import "../style/css/pages/teams.scss";
 
 const Teams = () => {
   const { currentUser } = useContext(AuthContext);
@@ -15,10 +17,13 @@ const Teams = () => {
       </div>
       <div className="new-team-container">
         <input type="text" placeholder="Team name" className="team-name" />
-        <Button />
+        <Select className="select" />
+        <Button
+          color="green"
+          button_icon={<AddIcon fontSize="inherit" />}
+          className="add"
+        />
       </div>
-
-      <Select />
     </div>
   );
 };
