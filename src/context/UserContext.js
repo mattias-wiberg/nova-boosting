bdda6fe3364efaf34c4147dbeb6c52f6bbd589d8
Mattias Wiberg <klutten99@gmail.com>
2023-03-10 16:51:52 +0100

    Added remove and add character methods.

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,12 +1,73 @@
 import { onAuthStateChanged } from "firebase/auth";
-import { doc, getDoc } from "firebase/firestore";
-import { createContext, useContext, useEffect, useState } from "react";
+import {
+  arrayRemove,
+  arrayUnion,
+  doc,
+  getDoc,
+  updateDoc,
+} from "firebase/firestore";
+import { createContext, useEffect, useState } from "react";
 import { auth, db } from "../firebase";
+import { getCharacter } from "../js/utilities";
 
 export const UserContext = createContext();
 
 export const UserContextProvider = ({ children }) => {
   const [user, setUser] = useState({});
+
+  const addCharacter = async (setError, characterText) => {
+    console.log("Add character");
+    const character = await getCharacter(setError, characterText);
+    // Check if character is valid
+    if (character === undefined) {
+      return;
+    }
+    // Update user with new character
+    try {
+      // Update user context
+      setUser((prevUser) => {
+        return {
+          ...prevUser,
+          characters: [...prevUser.characters, character],
+          character_priority: [...prevUser.character_priority, character.id],
+        };
+      });
+      // Update user in firestore
+      const userRef = doc(db, "users", user.uid);
+      await updateDoc(userRef, {
+        characters: arrayUnion(character),
+        character_priority: arrayUnion(character.id),
+      });
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
+  const removeCharacter = async (character, setError) => {
+    console.log("Remove character with id:", character.id);
+
+    // Remove character from user
+    try {
+      // Update user context
+      setUser((prevUser) => {
+        return {
+          ...prevUser,
+          characters: prevUser.characters.filter((c) => c.id !== character.id),
+          character_priority: prevUser.character_priority.filter(
+            (c) => c.id !== character.id
+          ),
+        };
+      });
+      // Update user in firestore
+      const userRef = doc(db, "users", user.uid);
+      await updateDoc(userRef, {
+        characters: arrayRemove(character),
+        character_priority: arrayRemove(character.id),
+      });
+    } catch (err) {
+      setError(err.message);
+    }
+  };
 
   useEffect(() => {
     const unsub = onAuthStateChanged(auth, (user) => {
@@ -18,6 +79,7 @@ export const UserContextProvider = ({ children }) => {
           setUser(docSnap.data());
         };
         fetchUser();
+        console.log("Fetched user data");
       } else {
         setUser({});
       }
@@ -29,6 +91,8 @@ export const UserContextProvider = ({ children }) => {
   }, []);
 
   return (
-    <UserContext.Provider value={{ user }}>{children}</UserContext.Provider>
+    <UserContext.Provider value={{ user, removeCharacter, addCharacter }}>
+      {children}
+    </UserContext.Provider>
   );
 };
