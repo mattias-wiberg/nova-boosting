81c75ae06412df8bb5ab15573a0e977a875cb0e6
Mattias Wiberg <klutten99@gmail.com>
2023-03-19 18:48:37 +0100

    Dungeons const var.

--- src/js/utilities.js
+++ src/js/utilities.js
@@ -1,5 +1,26 @@
 import { classArmor, classRole } from "./maps.js";
 import { v4 as uuid } from "uuid";
+import AA_BG from "../img/dungeons/background/AA.jpg";
+import CoS_BG from "../img/dungeons/background/CoS.jpg";
+import HoV_BG from "../img/dungeons/background/HoV.jpg";
+import RLP_BG from "../img/dungeons/background/RLP.jpg";
+import SBG_BG from "../img/dungeons/background/SBG.jpg";
+import TJS_BG from "../img/dungeons/background/TJS.jpg";
+import AV_BG from "../img/dungeons/background/AV.jpg";
+import NO_BG from "../img/dungeons/background/NO.jpg";
+import ANY from "../img/dungeons/background/ANY.jpg";
+
+export const DUNGEONS = {
+  AA: { name: "Algeth'ar Academy", img: AA_BG },
+  CoS: { name: "Court of Stars", img: CoS_BG },
+  HoV: { name: "Halls of Valor", img: HoV_BG },
+  RLP: { name: "Ruby Life Pools", img: RLP_BG },
+  SBG: { name: "Shadowmoon Burial Grounds", img: SBG_BG },
+  TJS: { name: "Temple of the Jade Serpent", img: TJS_BG },
+  AV: { name: "The Azure Vault", img: AV_BG },
+  NO: { name: "The Nokhud Offensive", img: NO_BG },
+  ANY: { name: "ANY", img: ANY },
+};
 
 export const getCharacter = async (setError, characterText) => {
   const region = "eu";
