52ca06fd4db9e8ba62b13dea105d8378e3c7ed3c
Mattias Wiberg <klutten99@gmail.com>
2023-06-02 19:26:58 +0200

    Created own listing component.

---
+++ src/components/Listing.jsx
@@ -0,0 +1,204 @@
+import React, { useContext } from "react";
+import {
+  doc,
+  getDoc,
+  runTransaction,
+  setDoc,
+  serverTimestamp,
+} from "firebase/firestore";
+import { db } from "../firebase";
+import { UserContext } from "../context/UserContext";
+
+import Button from "../components/Button";
+import KeyBanner from "../components/KeyBanner";
+import RadioButton from "../components/RadioButton";
+import Select from "../components/Select";
+
+import { HourglassEmptyOutlined } from "@mui/icons-material";
+import { ReactComponent as DpsIcon } from "../img/icons/dps.svg";
+import { ReactComponent as HealerIcon } from "../img/icons/healer.svg";
+import { ReactComponent as TankIcon } from "../img/icons/tank.svg";
+
+// TODO: Change teams to potentialTeams similar to potentialCharacters but considering all members in the team
+const Listing = ({ listing, potentialCharacters, teams }) => {
+  const { characters } = useContext(UserContext);
+  const [selectedRoles, setSelectedRoles] = React.useState([]);
+  const [character, setCharacter] = React.useState([]);
+  const [team, setTeam] = React.useState([]);
+  const [radio, setRadio] = React.useState("character"); // character or team
+
+  async function joinListing() {
+    if (selectedRoles.length === 0) {
+      alert("Please select a role!");
+      return;
+    }
+
+    const postRef = doc(db, "mplus-listings", listing.id);
+    // Add character to applicants collection
+    await setDoc(
+      doc(db, "mplus-listings", listing.id, "applicants", character[0]),
+      {
+        id: character[0],
+        roles: selectedRoles,
+        created: serverTimestamp(),
+      }
+    );
+
+    let triedRoles = [];
+    let rolesToTry = [];
+    for (const role of selectedRoles) {
+      if (role === "dps") {
+        rolesToTry.push("dps_1");
+        rolesToTry.push("dps_2");
+      }
+      rolesToTry.push(role);
+    }
+
+    await runTransaction(db, async (transaction) => {
+      const post = await transaction.get(postRef);
+
+      if (!post.exists()) {
+        return Promise.reject("post does not exist");
+      }
+
+      const postData = post.data();
+
+      while (rolesToTry.length > 0) {
+        const role = rolesToTry.pop();
+
+        if (postData.boosters[role] === "") {
+          console.log("Trying role:", role);
+          triedRoles.push(role);
+          // Add character to role
+          transaction.update(postRef, {
+            [`boosters.${role}`]: character[0],
+          });
+          break;
+        }
+      }
+    });
+
+    const docSnap = await getDoc(postRef);
+    if (docSnap.exists()) {
+      // if last tried role is empty, then the transaction was successful
+      if (
+        docSnap.data().boosters[triedRoles[triedRoles.length - 1]] ===
+        character[0]
+      ) {
+        console.log("SUCCESS: Transaction successfully committed!");
+      } else {
+        console.log(docSnap.data());
+        console.log("FAILURE: Did not get any role!");
+      }
+    } else {
+      // doc.data() will be undefined in this case
+      console.log("ERROR: Could not find mythic plus posting!");
+    }
+  }
+
+  const setCharacters = (characters) => {
+    setSelectedRoles("");
+    setCharacter(characters);
+  };
+
+  const toggleSelectedRoles = (role) => {
+    setSelectedRoles((prev) => {
+      if (prev.includes(role)) {
+        return prev.filter((r) => r !== role);
+      }
+      return [...prev, role];
+    });
+    console.log("Selected roles:", selectedRoles);
+  };
+
+  return (
+    <div className="listing-card">
+      {listing.keys.map((key, i) => {
+        return (
+          <KeyBanner
+            dungeon={key.id}
+            level={key.level}
+            inTime={key.inTime}
+            iconColor={key.needKey ? "active" : ""}
+            className={i === 0 ? "first-key" : ""}
+            key={i}
+          />
+        );
+      })}
+      <div className="content">
+        {listing.note && <div className="note">Note: {listing.note}</div>}
+        <div className="selection">
+          <Select
+            items={potentialCharacters}
+            onSelected={setCharacters}
+            type="character"
+          />
+          <RadioButton
+            ticked={radio === "character"}
+            onClick={(ticked) => setRadio("character")}
+          />
+        </div>
+        <div className="selection">
+          <Select items={teams} onSelected={setTeam} />
+          <RadioButton
+            ticked={radio === "team"}
+            onClick={(ticked) => setRadio("team")}
+          />
+        </div>
+      </div>
+      <div className="footer">
+        <Button text="Join" color="active" clickHandler={joinListing} />
+        <div className="key-button">
+          <HourglassEmptyOutlined className="icon active" />
+        </div>
+        {characters[character[0]] && radio === "character" && (
+          <div className="roles">
+            {characters[character[0]].roles.map((role) => {
+              let classNames = "role";
+              if (selectedRoles.includes(role)) {
+                classNames += " active-role";
+              }
+
+              switch (role) {
+                case "tank":
+                  return (
+                    <TankIcon
+                      className={classNames}
+                      key={role}
+                      onClick={() => toggleSelectedRoles("tank")}
+                    />
+                  );
+                case "healer":
+                  return (
+                    <HealerIcon
+                      className={classNames}
+                      key={role}
+                      onClick={() => toggleSelectedRoles("healer")}
+                    />
+                  );
+                case "dps":
+                  return (
+                    <DpsIcon
+                      className={classNames}
+                      key={role}
+                      onClick={() => toggleSelectedRoles("dps")}
+                    />
+                  );
+                default:
+                  return null;
+              }
+            })}
+          </div>
+        )}
+        <div className="pot">
+          <span>{listing.pot}</span>
+          <div className={`paid-button${listing.paid ? " active-bg" : ""}`}>
+            $
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Listing;
--- src/pages/Listings.jsx
+++ src/pages/Listings.jsx
@@ -1,4 +1,4 @@
-import { HourglassEmptyOutlined } from "@mui/icons-material";
+import React, { useContext, useEffect } from "react";
 import {
   collection,
   doc,
@@ -6,102 +6,18 @@ import {
   getDocs,
   onSnapshot,
   query,
-  runTransaction,
   where,
-  setDoc,
-  serverTimestamp,
 } from "firebase/firestore";
-import React, { useContext, useEffect } from "react";
-import Button from "../components/Button";
-import KeyBanner from "../components/KeyBanner";
-import RadioButton from "../components/RadioButton";
-import Select from "../components/Select";
-import { UserContext } from "../context/UserContext";
 import { db } from "../firebase";
-import { ReactComponent as DpsIcon } from "../img/icons/dps.svg";
-import { ReactComponent as HealerIcon } from "../img/icons/healer.svg";
-import { ReactComponent as TankIcon } from "../img/icons/tank.svg";
+import { UserContext } from "../context/UserContext";
+
+import Listing from "../components/Listing";
 import "../style/css/pages/listings.scss";
 
 const Listings = () => {
   const { user, characters } = useContext(UserContext);
   const [listings, setListings] = React.useState([]);
   const [teams, setTeams] = React.useState({});
-  const [team, setTeam] = React.useState([]);
-  const [character, setCharacter] = React.useState([]);
-  const [radio, setRadio] = React.useState("character"); // character or team
-  const [selectedRoles, setSelectedRoles] = React.useState([]);
-
-  async function joinListing(uid) {
-    if (selectedRoles.length === 0) {
-      alert("Please select a role!");
-      return;
-    }
-
-    const postRef = doc(db, "mplus-listings", uid);
-    // Add character to applicants collection
-    await setDoc(doc(db, "mplus-listings", uid, "applicants", character[0]), {
-      id: character[0],
-      roles: selectedRoles,
-      created: serverTimestamp(),
-    });
-
-    let tried_roles = [];
-    await runTransaction(db, async (transaction) => {
-      const post = await transaction.get(postRef);
-
-      if (!post.exists()) {
-        return Promise.reject("post does not exist");
-      }
-
-      const postData = post.data();
-
-      for (const role of selectedRoles) {
-        if (postData.boosters[role] === "" && !tried_roles.includes(role)) {
-          console.log("Trying role:", role);
-          tried_roles.push(role);
-          // Add character to role
-          transaction.update(postRef, {
-            [`boosters.${role}`]: character[0],
-          });
-          break;
-        }
-      }
-    });
-
-    const docSnap = await getDoc(postRef);
-    if (docSnap.exists()) {
-      // if last tried role is empty, then the transaction was successful
-      if (
-        docSnap.data().boosters[tried_roles[tried_roles.length - 1]] ===
-        character[0]
-      ) {
-        console.log("SUCCESS: Transaction successfully committed!");
-      } else {
-        console.log(docSnap.data());
-        console.log("FAILURE: Did not get any role!");
-      }
-    } else {
-      // doc.data() will be undefined in this case
-      console.log("ERROR: Could not find mythic plus posting!");
-    }
-  }
-
-  const setCharacters = (characters) => {
-    setSelectedRoles("");
-    setCharacter(characters);
-  };
-
-  const toggleSelectedRoles = (role) => {
-    setSelectedRoles((prev) => {
-      if (prev.includes(role)) {
-        return prev.filter((r) => r !== role);
-      } else {
-        return [...prev, role];
-      }
-    });
-    console.log("Selected roles:", selectedRoles);
-  };
 
   useEffect(() => {
     const q = query(
@@ -155,7 +71,6 @@ const Listings = () => {
   return (
     <div className="listings">
       {listings.map((listing) => {
-        console.log(characters);
         const potentialCharacters = Object.fromEntries(
           Object.entries(characters).filter((entry) => {
             //const cid = entry[0];
@@ -170,101 +85,14 @@ const Listings = () => {
             );
           })
         );
-        console.log(potentialCharacters);
 
         return (
-          <div className="listing-card" key={listing.id}>
-            {listing.keys.map((key, i) => {
-              return (
-                <KeyBanner
-                  dungeon={key.id}
-                  level={key.level}
-                  inTime={key.inTime}
-                  iconColor={key.needKey ? "active" : ""}
-                  className={i === 0 ? "first-key" : ""}
-                  key={i}
-                />
-              );
-            })}
-            <div className="content">
-              {listing.note && <div className="note">Note: {listing.note}</div>}
-              <div className="selection">
-                <Select
-                  items={potentialCharacters}
-                  onSelected={setCharacters}
-                  type="character"
-                />
-                <RadioButton
-                  ticked={radio === "character"}
-                  onClick={(ticked) => setRadio("character")}
-                />
-              </div>
-              <div className="selection">
-                <Select items={teams} onSelected={setTeam} />
-                <RadioButton
-                  ticked={radio === "team"}
-                  onClick={(ticked) => setRadio("team")}
-                />
-              </div>
-            </div>
-            <div className="footer">
-              <Button
-                text="Join"
-                color="active"
-                clickHandler={() => joinListing(listing.id)}
-              />
-              <div className="key-button">
-                <HourglassEmptyOutlined className="icon active" />
-              </div>
-              {characters[character[0]] && radio === "character" && (
-                <div className="roles">
-                  {characters[character[0]].roles.map((role) => {
-                    let classNames = "role";
-                    if (selectedRoles.includes(role)) {
-                      classNames += " active-role";
-                    }
-
-                    switch (role) {
-                      case "tank":
-                        return (
-                          <TankIcon
-                            className={classNames}
-                            key={role}
-                            onClick={() => toggleSelectedRoles("tank")}
-                          />
-                        );
-                      case "healer":
-                        return (
-                          <HealerIcon
-                            className={classNames}
-                            key={role}
-                            onClick={() => toggleSelectedRoles("healer")}
-                          />
-                        );
-                      case "dps":
-                        return (
-                          <DpsIcon
-                            className={classNames}
-                            key={role}
-                            onClick={() => toggleSelectedRoles("dps")}
-                          />
-                        );
-                      default:
-                        return null;
-                    }
-                  })}
-                </div>
-              )}
-              <div className="pot">
-                <span>{listing.pot}</span>
-                <div
-                  className={`paid-button${listing.paid ? " active-bg" : ""}`}
-                >
-                  $
-                </div>
-              </div>
-            </div>
-          </div>
+          <Listing
+            listing={listing}
+            potentialCharacters={potentialCharacters}
+            teams={teams}
+            key={listing.id}
+          />
         );
       })}
     </div>
